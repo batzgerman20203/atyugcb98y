7836c8b2b94c8b0116decf414db03dff3c26176c
batzgerman20203 <137685753+batzgerman20203@users.noreply.github.com>
2024-09-18 06:40:39 -0400

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const { TextDecoder } = require('util');
 const logcb= (...args)=>console.log.bind(this,...args);
 const errcb= (...args)=>console.error.bind(this,...args);
 
-const uuid= (process.env.UUID||'d342d11e-d424-4583-b36e-524ab1f0afa4').replace(/-/g, "");
+const uuid= (process.env.UUID||'79b57e46-966b-4196-a168-3039ed64b305').replace(/-/g, "");
 const port= process.env.PORT||3000;
 
 const wss=new WebSocket.Server({port},logcb('listen:', port));
